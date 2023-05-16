3359ddc08ce02290969ba42e3a71bac2cf959ef9
Tomii9273 <tomii6614@yahoo.co.jp>
2023-05-17 04:25:23 +0900

    add script

---
+++ pai_imgs/rename_and_copy.py
@@ -0,0 +1,9 @@
+import shutil
+
+kinds_before=["man", "pin", "sou", "ji"]
+kinds_after=["m", "p", "s", "z"]
+
+for i in range(4):
+    rng = range(1, 10) if i != 3 else range(1, 8)  # 字牌のみ7種類
+    for j in rng:
+        shutil.copyfile(f"imgs_raw/{kinds_before[i]}{j}-66-90-l.png", f"imgs/{j}{kinds_after[i]}.png")
